916bdb71d42d45e5d50c39fde1e99dc504271b3b
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-10-27 15:43:54 +0900

    Add initial Windows runner and Flutter integration

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('저를 소개합니다.'),
+          backgroundColor: Colors.white,
+        ),
+        body: Column(
+          children: [
+            const ProfileCard(),
+            DescriptionCard(title: '', description: ''),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //TODO: 여기에 프로필 카드 구현
+    );
+  }
+}
+
+class DescriptionCard extends StatelessWidget {
+  const DescriptionCard({
+    super.key,
+    required this.title,
+    required this.description,
+    this.color,
+  });
+  final String title;
+  final String description;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //TODO: 여기에 Description Card 구현
+    );
+  }
+}
